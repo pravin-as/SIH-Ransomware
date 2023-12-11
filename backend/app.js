78ae6914a3a5828abc4f0f35831ca971ceba7a9c
= <=>
2023-12-11 18:12:54 +0530

    initial setup

---
+++ backend/app.js
@@ -0,0 +1,54 @@
+const express = require('express') ; 
+const cookieParser = require('cookie-parser') ; 
+const app = express() ; 
+const bodyParser = require('body-parser') ; 
+const fileUpload =  require("express-fileupload") ;  
+
+app.use(cookieParser()) ; 
+app.use(express.json()) ; 
+app.use(bodyParser.urlencoded({extended: true})) ;   
+
+
+// adding control for backend CORS resolution 
+app.use((res , req ,next)=>{ 
+
+    // Set the allowed origins
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your frontend domain
+  
+    // Set the allowed methods
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  
+    // Set the allowed headers
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+    // Allow credentials (cookies, authorization headers) to be sent
+    res.header('Access-Control-Allow-Credentials', true);
+  
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      return res.status(200).json({});
+    }
+  
+    next();
+
+}) ; 
+
+
+app.use(fileUpload()) ; 
+
+app.use(function(req  ,res , next){ 
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+}) ; 
+
+const errorMiddleware = require('./middleware/error.js') ;    
+
+
+module.exports= app ; 
+
+
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const app =  require('./app') ; 
+const dotenv = require("dotenv") ; 
+const cors = require("cors") ; 
+const morgan = require("morgan") ; 
+
+dotenv.config({path: "D:\\SIH-Ransomware\\backend\\config.env"}) ; 
+ 
+app.options("*", cors({ origin: 'http://localhost:3000', optionsSuccessStatus: 200 }));
+
+app.use(cors({ origin: "http://localhost:3000", optionsSuccessStatus: 200 }));
+app.use(cors({origin: true, credentials: true}));  
+app.use(morgan('tiny')) ;   
+
+
+
+process.on("uncaughtException" , (err)=>{ 
+      console.log(err.message) ; 
+      console.log("shutting Down the server due to uncaught Exception") ; 
+      server.close(()=>{
+         process.exit(1) ; 
+      })
+}) ;   
+
+
+app.listen(process.env.PORT || 3000 , ()=>{ 
+     console.log(`server is working on http://localhost:${process.env.PORT}`) 
+
+}) ; 
